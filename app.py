fbd8b5f58bda494ff1b82fc6d3d1b2ce5f1f1872
Adithak99 <95956891+Adithak99@users.noreply.github.com>
2023-01-14 21:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://Adithak99:github.com/Adithak99/storex0 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Adithak99: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@github.com/Adithak99/storex0 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
